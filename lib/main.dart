f1f80ad017c29c732f9b4ced1d4057e6e8438fdd
rahman9868 <rahman9868@gmail.com>
2026-01-13 08:55:53 +0000

    sqflite3

--- lib/main.dart
+++ lib/main.dart
@@ -4,23 +4,24 @@ import 'package:myapp/app/data/local/app_database.dart';
 import 'package:myapp/app/routes/app_pages.dart';
 import 'package:sqflite_common_ffi/sqflite_ffi.dart';
 import 'dart:developer' as developer;
+import 'dart:io';
 
 void main() async {
   // Ensure all Flutter bindings are ready.
   WidgetsFlutterBinding.ensureInitialized();
 
-  // Initialize FFI for sqflite on desktop platforms.
-  sqfliteFfiInit();
-
-  // Set the database factory to the FFI implementation.
-  databaseFactory = databaseFactoryFfi;
+  // Conditionally initialize sqflite for desktop platforms.
+  if (Platform.isWindows || Platform.isLinux || Platform.isMacOS) {
+    sqfliteFfiInit();
+    databaseFactory = databaseFactoryFfi;
+  }
 
   try {
     // Now, we can safely build the database.
     final database = await $FloorAppDatabase
         .databaseBuilder('app_database.db')
         .build();
-    Get.put(database); // Register the database instance with GetX.
+    Get.put<AppDatabase>(database); // Register the database instance with GetX.
   } catch (e, s) {
     // Log the error for better debugging.
     developer.log(
