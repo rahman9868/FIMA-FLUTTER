91a51b44690915314a50bb14d77879a705083878
rahman9868 <rahman9868@gmail.com>
2026-01-15 09:44:10 +0000

    fix

--- lib/app/modules/main/bindings/main_binding.dart
+++ lib/app/modules/main/bindings/main_binding.dart
@@ -3,8 +3,10 @@ import 'package:myapp/app/data/local/app_database.dart';
 import 'package:myapp/app/data/providers/api_provider.dart';
 import 'package:myapp/app/data/providers/cache_manager.dart';
 import 'package:myapp/app/data/repositories/dashboard_repository.dart';
+import 'package:myapp/app/data/repositories/my_report_repository.dart';
 import 'package:myapp/app/modules/dashboard/controllers/dashboard_controller.dart';
 import 'package:myapp/app/modules/main/controllers/main_controller.dart';
+import 'package:myapp/app/modules/my_report/controllers/my_report_controller.dart';
 
 class MainBinding extends Bindings {
   @override
@@ -12,10 +14,10 @@ class MainBinding extends Bindings {
     Get.lazyPut<CacheManager>(() => CacheManager(), fenix: true);
     Get.lazyPut<ApiProvider>(() => ApiProvider(), fenix: true);
 
+    // Dashboard Dependencies
     Get.lazyPut<DashboardRepository>(
       () => DashboardRepository(Get.find<AppDatabase>()),
     );
-
     Get.lazyPut<DashboardController>(
       () => DashboardController(
         repository: Get.find(),
@@ -23,6 +25,18 @@ class MainBinding extends Bindings {
       ),
     );
 
+    // My Report Dependencies
+    Get.lazyPut<MyReportRepository>(
+      () => MyReportRepository(),
+    );
+    Get.lazyPut<MyReportController>(
+      () => MyReportController(
+        repository: Get.find(),
+        cacheManager: Get.find(),
+      ),
+    );
+
+    // Main Controller
     Get.lazyPut<MainController>(
       () => MainController(
         cacheManager: Get.find(),
